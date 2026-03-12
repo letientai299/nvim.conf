b5e1fea9b7539f545a8eb36f84e6bf9c9cc99c47
Tai <letientai299@gmail.com>
2026-03-12 22:08:41 +0700

    feat: add postgres LSP and pgFormatter for SQL
    Replace sql-formatter with pgFormatter and enable
    postgres-language-server for SQL files.

--- lua/langs/sql.lua
+++ lua/langs/sql.lua
@@ -1,12 +1,15 @@
 local M = {}
 
-function M.setup()
+function M.setup(bufnr)
   require("lib.tools").check_now({
-    { name = "sql-formatter", bin = "sql-formatter", kind = "fmt" },
+    { name = "postgres-language-server", bin = "postgres-language-server", kind = "lsp" },
+    { name = "pgFormatter", bin = "pg_format", kind = "fmt" },
   })
 
+  require("lib.lsp").enable("postgres_lsp", bufnr)
+
   local registry = require("lib.lang_registry")
-  registry.add_formatters("sql", { "sql_formatter" })
+  registry.add_formatters("sql", { "pg_format" })
   registry.ensure_parsers({ "sql" })
 end
 
