92eb36fb5843d7b276395f8c6e4cb349e4b8d731
Tai <letientai299@gmail.com>
2026-03-12 16:47:30 +0700

    fix: restore astro LSP with correct tsdk init_options

--- lua/langs/astro.lua
+++ lua/langs/astro.lua
@@ -1,4 +1,19 @@
+require("lib.tools").check("astro", {
+  { name = "astro-ls", bin = "astro-ls", kind = "lsp" },
+  { name = "prettier", bin = "prettier", kind = "fmt" },
+})
+
+vim.lsp.enable("astro")
+
 return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        astro = { "prettier" },
+      },
+    },
+  },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = { ensure_installed = { "astro" } },
