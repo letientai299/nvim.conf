f2521202c98199f2c6b9066864501da869682e51
Tai <letientai299@gmail.com>
2026-03-12 22:28:40 +0700

    fix: resolve undefined global vim warnings from lua_ls
    lua_ls didn't know about Neovim's runtime because lazydev's
    lspconfig integration doesn't fire with native vim.lsp.enable().
    Add explicit settings and luv library path for vim.uv types.

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -2,4 +2,14 @@ return {
   cmd = { "lua-language-server" },
   filetypes = { "lua" },
   root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" },
+      diagnostics = { globals = { "vim" } },
+      workspace = {
+        checkThirdParty = false,
+        library = { vim.env.VIMRUNTIME }, ---@diagnostic disable-line: undefined-global
+      },
+    },
+  },
 }
--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -2,6 +2,9 @@ return {
   "folke/lazydev.nvim",
   ft = "lua",
   opts = {
+    library = {
+      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    },
     enabled = function(root_dir)
       return not vim.uv.fs_stat(root_dir .. "/.luarc.json")
     end,
