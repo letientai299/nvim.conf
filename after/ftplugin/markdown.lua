ea04899a7f96b8dc2a1005ff91ff09ba46f19b6c
Tai <letientai299@gmail.com>
2026-03-12 22:11:27 +0700

    feat: add AutoCopy command and markdown checklist toggle
    :AutoCopy tracks one buffer and copies content to clipboard on
    focus/buffer leave. <C-Space> in markdown toggles checklist state.

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,27 @@
+local function toggle_line(lnum)
+  local line = vim.api.nvim_buf_get_lines(0, lnum - 1, lnum, false)[1]
+  if line:match("^(%s*)%- %[x%]") then
+    line = line:gsub("^(%s*)%- %[x%]", "%1- [ ]")
+  elseif line:match("^(%s*)%- %[ %]") then
+    line = line:gsub("^(%s*)%- %[ %]", "%1- [x]")
+  elseif line:match("^(%s*)%- ") then
+    line = line:gsub("^(%s*)%- (.*)", "%1- [ ] %2")
+  end
+  vim.api.nvim_buf_set_lines(0, lnum - 1, lnum, false, { line })
+end
+
+vim.keymap.set("n", "<C-Space>", function()
+  toggle_line(vim.fn.line("."))
+end, { buffer = true, desc = "Toggle checklist" })
+
+vim.keymap.set("x", "<C-Space>", function()
+  local start = vim.fn.line("v")
+  local stop = vim.fn.line(".")
+  if start > stop then
+    start, stop = stop, start
+  end
+  for lnum = start, stop do
+    toggle_line(lnum)
+  end
+  vim.cmd("normal! \27") -- exit visual mode
+end, { buffer = true, desc = "Toggle checklist" })
