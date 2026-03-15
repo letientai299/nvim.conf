5ad0d44293963258ee66958d642e4e8ab2f28d88
Tai <letientai299@gmail.com>
2026-03-16 05:27:41 +0700

    test: make `install` use `-y` to test faster

--- tests/infra/bashrc.sh
+++ tests/infra/bashrc.sh
@@ -4,5 +4,5 @@ PS1='\[\033[1;32m\]\u@\h\[\033[0m\] \[\033[1;34m\]\w\[\033[0m\]\n\$ '
 export PS1
 
 alias v=nvim vi=nvim vim=nvim
-alias install='~/work/scripts/install.sh'
+alias install='~/work/scripts/install.sh -y'
 alias se='source ~/.bashrc'
