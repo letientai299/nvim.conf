57c70ef07ce63a7880daabb8821b52a3233165f6
Tai <letientai299@gmail.com>
2026-03-18 02:00:20 +0700

    feat: compact lualine statusline
    Drop filename and filetype from statusline (tabline covers both).
    Shorten mode to single letter, show LSP icon instead of client
    names, add reading progress, and show relative paths in tabline
    buffers for disambiguation.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,10 @@
+local function lsp_active()
+  if not next(vim.lsp.get_clients({ bufnr = 0 })) then
+    return ""
+  end
+  return ""
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
@@ -9,11 +16,24 @@ return {
         component_separators = "",
       },
       tabline = {
-        lualine_a = { { "buffers", show_filename_only = true, mode = 2 } },
+        lualine_a = {
+          {
+            "buffers",
+            show_filename_only = false,
+            mode = 2,
+          },
+        },
         lualine_z = { "tabs" },
       },
       sections = {
-        lualine_a = { "mode" },
+        lualine_a = {
+          {
+            "mode",
+            fmt = function(s)
+              return s:sub(1, 1)
+            end,
+          },
+        },
         lualine_b = {
           "branch",
           {
@@ -31,12 +51,12 @@ return {
             end,
           },
         },
-        lualine_c = { { "filename", path = 1 }, "aerial", "searchcount" },
+        lualine_c = { "aerial", "searchcount" },
         lualine_x = {
-          { "lsp_status", symbols = { done = "", separator = " " } },
+          lsp_active,
           "diagnostics",
         },
-        lualine_y = { "filetype" },
+        lualine_y = { "progress" },
         lualine_z = { "selectioncount", "location" },
       },
       extensions = { "oil", "toggleterm", "quickfix", "fzf", "lazy" },
