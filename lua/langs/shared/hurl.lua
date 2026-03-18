acc6b3f77fd5a47dec4453102989a7f473ec966f
Tai <letientai299@gmail.com>
2026-03-18 20:51:12 +0700

    feat: add hurlfmt as linter for hurl files
    Define a custom nvim-lint linter that parses hurlfmt parse errors
    with line/col positions into diagnostics.

--- lua/langs/shared/hurl.lua
+++ lua/langs/shared/hurl.lua
@@ -4,6 +4,30 @@ function M.setup(bufnr)
   require("langs.shared.entry").setup("hurl", bufnr, {
     tools = { { bin = "hurlfmt", mise = "hurl" } },
     formatters = { "hurlfmt" },
+    linters = { "hurlfmt" },
+    once = function()
+      -- hurlfmt parse errors: error: <msg>\n  --> <file>:<line>:<col>
+      require("lint").linters.hurlfmt = {
+        cmd = "hurlfmt",
+        stdin = true,
+        stream = "stderr",
+        parser = function(output)
+          local diags = {}
+          for msg, line, col in
+            output:gmatch("error: ([^\n]+)\n%s*%-%-> [^:]+:(%d+):(%d+)")
+          do
+            diags[#diags + 1] = {
+              lnum = tonumber(line) - 1,
+              col = tonumber(col) - 1,
+              severity = vim.diagnostic.severity.ERROR,
+              source = "hurlfmt",
+              message = msg,
+            }
+          end
+          return diags
+        end,
+      }
+    end,
   })
 end
 
