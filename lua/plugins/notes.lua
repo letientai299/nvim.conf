2a089a9e839f9f0d20bdf0b918c3d6f1ecb0a78e
Tai <letientai299@gmail.com>
2026-03-21 17:05:26 +0700

    fix: LocalTodo keymap wasn't registered properly

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -1,13 +1,6 @@
 return {
   dir = vim.fn.stdpath("config") .. "/plugins/notes.nvim",
-  cmd = "LocalTodo",
-  keys = {
-    {
-      "<Leader>td",
-      "<Cmd>LocalTodo<CR>",
-      desc = "Open local todo",
-    },
-  },
+  cmd = "NoteToday",
   config = function()
     vim.api.nvim_create_user_command("NoteToday", function()
       require("notes").note_today()
