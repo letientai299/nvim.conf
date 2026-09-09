659719cb369c81bcb20cb48bcce2f7597cb30c05
Tai Le <taile@nvidia.com>
2026-09-09 07:57:19 +0700

    chore: use diff2_vertical in diffview as I use narrow term width

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -15,4 +15,12 @@ return {
   keys = {
     { "<Leader>gD", "<Cmd>DiffviewOpen<CR>", desc = "Diff view" },
   },
+  config = function()
+    require("diffview").setup({
+      view = {
+        default = { layout = "diff2_vertical" },
+        file_history = { layout = "diff2_vertical" },
+      },
+    })
+  end,
 }
